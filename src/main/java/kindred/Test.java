4506c05188d0bd4d7eb757673b9aeee2696db1f5
tangye <109327586@qq.com>
2024-04-12 17:22:25 +0800

    add mir lib test in processing

--- src/main/java/kindred/Test.java
+++ src/main/java/kindred/Test.java
@@ -1,19 +1,28 @@
 package kindred;
 
 import com.sun.jna.NativeLibrary;
+import com.sun.jna.Pointer;
 import io.github.libsdl4j.api.event.SDL_Event;
+
+import io.github.libsdl4j.api.image.SDL_imageLibrary;
+import io.github.libsdl4j.api.rect.SDL_Rect;
 import io.github.libsdl4j.api.render.SDL_Renderer;
+import io.github.libsdl4j.api.render.SDL_Texture;
+import io.github.libsdl4j.api.rwops.SDL_RWops;
 import io.github.libsdl4j.api.video.SDL_Window;
 
 import static io.github.libsdl4j.api.Sdl.SDL_Init;
 import static io.github.libsdl4j.api.Sdl.SDL_Quit;
 import static io.github.libsdl4j.api.SdlSubSystemConst.SDL_INIT_EVERYTHING;
+import static io.github.libsdl4j.api.blendmode.SDL_BlendMode.SDL_BLENDMODE_BLEND;
 import static io.github.libsdl4j.api.error.SdlError.SDL_GetError;
 import static io.github.libsdl4j.api.event.SDL_EventType.*;
 import static io.github.libsdl4j.api.event.SdlEvents.SDL_PollEvent;
 import static io.github.libsdl4j.api.keycode.SDL_Keycode.SDLK_SPACE;
 import static io.github.libsdl4j.api.render.SDL_RendererFlags.SDL_RENDERER_ACCELERATED;
 import static io.github.libsdl4j.api.render.SdlRender.*;
+import static io.github.libsdl4j.api.rwops.SdlRWops.SDL_RWFromConstMem;
+import static io.github.libsdl4j.api.rwops.SdlRWops.SDL_RWclose;
 import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_RESIZABLE;
 import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_SHOWN;
 import static io.github.libsdl4j.api.video.SdlVideo.SDL_CreateWindow;
@@ -21,7 +30,7 @@ import static io.github.libsdl4j.api.video.SdlVideoConst.SDL_WINDOWPOS_CENTERED;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         NativeLibrary.addSearchPath("SDL2", "/home/kindred/mywork/projects/java/java_sdl/libsdl4j/src/main/resources/linux-x86-64");
         // Initialize SDL
         int result = SDL_Init(SDL_INIT_EVERYTHING);
@@ -41,8 +50,31 @@ public class Test {
             throw new IllegalStateException("Unable to create SDL renderer: " + SDL_GetError());
         }
 
+        //read from mirlib
+        MirLib mir_lib =new MirLib("/home/kindred/mywork/projects/cpp/devilutionX_kindred/my_asset/Prguse2_png.Lib");
+        mir_lib.Initialize();
+        MirImage img = mir_lib.GetMirImage(542);
+        Pointer data_p=new Pointer(img.header.length);
+        System.out.println("----------------0000------------");
+        data_p.write(0, img.data, 0, img.data.length);
+        System.out.println("----------------1111------------");
+        SDL_RWops rwops = SDL_RWFromConstMem(data_p, img.header.length);
+        System.out.println("----------------2222------------");
+        io.github.libsdl4j.api.surface.SDL_Surface surface = SDL_imageLibrary.INSTANCE.IMG_LoadPNG_RW(rwops);
+        SDL_RWclose(rwops);
+        SDL_Texture testTexture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_SetTextureColorMod(testTexture, (byte)255, (byte)0, (byte)255);
+        SDL_SetTextureBlendMode(testTexture, SDL_BLENDMODE_BLEND);
+        SDL_SetTextureAlphaMod(testTexture, (byte)255);
+        SDL_Rect dst = new SDL_Rect();
+        dst.x=0;
+        dst.y=0;
+        dst.w=surface.getW();
+        dst.h=surface.getH();
+        SDL_RenderCopy(renderer, testTexture, null, dst);
+
         // Set color of renderer to green
-        SDL_SetRenderDrawColor(renderer, (byte) 0, (byte) 255, (byte) 0, (byte) 255);
+        //SDL_SetRenderDrawColor(renderer, (byte) 0, (byte) 255, (byte) 0, (byte) 255);
 
         // Clear the window and make it all red
         SDL_RenderClear(renderer);
